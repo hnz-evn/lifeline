321c83bfa6153d6ab0f94fe338d8a9095704bdba
Evan Hunzinger <hnz.evn@gmail.com>
2019-08-11 11:51:09 -0500

    Add body-parser and request-id middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const requestId = require('express-request-id');
 const logger = require('./common/logger');
 
 const app = express();
 const port = 3000;
+
+app.use(requestId());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
