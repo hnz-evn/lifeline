a89aeade41439899c18fb4ed5792b6d88e03003f
Evan Hunzinger <hnz.evn@gmail.com>
2019-08-28 22:31:15 -0500

    Add functionality to login page

--- client/lifeline-client/src/views/Login.vue
+++ client/lifeline-client/src/views/Login.vue
@@ -3,24 +3,43 @@
     <div class="field">
       <label class="label">Email</label>
       <div class="control">
-        <input class="input" type="email" placeholder="Email">
+        <input class="input" type="email" placeholder="Email" v-model="email">
       </div>
     </div>
     <div class="field">
       <label class="label">Password</label>
       <div class="control">
-        <input class="input" type="password">
+        <input class="input" type="password" placeholder="Password" v-model="password">
       </div>
     </div>
     <div class="button-container">
-      <button class="button is-primary">Submit</button>
+      <button class="button is-primary" @click="submitLogin">Submit</button>
     </div>
   </div>
 </template>
 
 <script>
+import log from '../plugins/logger';
+import { login } from '../api/lifeline';
+
 export default {
   name: 'Login',
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+  methods: {
+    submitLogin() {
+      if (!this.email || !this.password) {
+        log.error('Must provide email and password');
+        return;
+      }
+
+      login({ email: this.email, password: this.password });
+    },
+  },
 };
 </script>
 
